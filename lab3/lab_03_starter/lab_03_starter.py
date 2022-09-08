f953111f26a34760b9400c3aaea86238ee6ab390
Thomas Waggoner <thomwagg@iu.edu>
2022-09-08 10:31:42 -0400

    main

---
+++ lab3/lab_03_starter/lab_03_starter.py
@@ -0,0 +1,18 @@
+import os
+
+
+# Part 1: write a short program that prints out the names of all of the contents 
+# of the examples/more_examples directory.
+
+
+
+
+
+#Part 2: Change the current working directory to the resources/code/ directory.
+# Print out the names of all of the files (not directories!) and their sizes
+
+
+
+
+
+#Part 3: Rename the functions.py file to database_functions.py.
